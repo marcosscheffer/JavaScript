af437cd726a8e6c138b7262a90146c092f47b4a8
Marcos Vinicius <marcosscheffer2989@gmail.com>
2021-11-21 23:26:47 -0300

    add

---
+++ ex003/script.js
@@ -0,0 +1,8 @@
+function verificar()
+    function clique() {
+        var date = new Date()
+        var now = date.getFullYear()
+        alert(now)
+
+        msg.innerHTML = `Você tem ${age} anos!`
+    }
