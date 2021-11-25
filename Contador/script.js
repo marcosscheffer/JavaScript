63c2c8b9b80c42ded34abb064ff0c010bb2783fd
Marcos Vinicius <marcosscheffer2989@gmail.com>
2021-11-25 15:41:54 -0300

    optimize

--- Contador/script.js
+++ Contador/script.js
@@ -3,7 +3,7 @@ function contar() {
     var num2 = document.getElementById("num2").value
     var seq = document.getElementById("seq").value
     var text = document.getElementById('text')
-    var array = new Array()
+    text.innerHTML = `Contando: `
 
     if (num1.length == 0) {
         alert("Por favor preencha o campo de COMEÇO!")
@@ -16,14 +16,12 @@ function contar() {
     num1 = Number(num1)
     num2 = Number(num2)
     seq = Number(seq)
-    if (num1 >= num2) {
+    if (num1 >= num2 || seq == 0) {
         text.innerHTML = `Impossivel fazer a contagem!`
-    } else if (num1.length != 0 && num2.length != 0 && seq.length != 0) {
-        while (num1 <= num2) {
-            array.push(num1)
-            num1 += seq
+    }else if (num1.length != 0 && num2.length != 0 && seq.length != 0) {
+        for (let c = num1; c <= num2; c += seq) {
+            text.innerHTML += `${c} \u{1F449}`
         }
-        text.innerHTML = `${array}`
+        text.innerHTML += `\u{1F3C1}`
     }
-    
 }
