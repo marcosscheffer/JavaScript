7dbc1fa5c46bf947088af77adc83a6decd21b644
Marcos Vinicius <marcosscheffer2989@gmail.com>
2021-11-26 20:29:15 -0300

    project

---
+++ Analizar Números/script.js
@@ -0,0 +1,70 @@
+let lista = []
+function add() {
+    let num = document.getElementById("num").value
+    let list = document.getElementById("list")
+
+    if (num.length == 0) {
+        alert("Campo vazio!")
+    } else if (num > 100) {
+        alert("Número muito grande!")
+    } else if (lista.length == 0) {
+        let num1 = document.createElement("option")
+        num1.text = `Valor ${num} adicionado!`
+        list.appendChild(num1)
+        lista.push(num)
+    } else if (lista.indexOf(num) == -1) {
+        let num1 = document.createElement("option")
+        num1.text = `Valor ${num} adicionado!`
+        list.appendChild(num1)
+        lista.push(num)
+    } else if (lista.indexOf(num) != -1) {
+        alert("Valor já adicionado!")
+    }
+}
+function analise() {
+    function maior(n) {
+        for (let c = 0; c < n.length; c++) {
+            if (c == 0) {
+                var maiorn = n[c]
+            } else {
+                if (maiorn < n[c]) {
+                    maiorn = n[c]
+                }
+            }
+        }
+        return maiorn
+    }
+    function menor(n) {
+        for (let c = 0; c < n.length; c++) {
+            if (c == 0) {
+                var menorn = n[c]
+            } else {
+                if (menorn > n[c]) {
+                    menorn = n[c]
+                }
+            }
+        }
+        return menorn
+    }
+    function media(n) {
+        let soma = 0
+        for (let c = 0; c < n.length; c++) {
+            soma += Number(n[c])
+        }
+        soma /= n.length
+        return soma
+    }
+    function soma(n) {
+        let soma = 0
+        for (let c = 0; c < n.length; c++) {
+            soma += Number(n[c])
+        }
+        return soma
+    }
+    let txt = document.getElementById("res")
+    txt.innerHTML += `O maior número é ${maior(lista)}<br>`
+    txt.innerHTML += `O menor número é ${menor(lista)}<br>`
+    txt.innerHTML += `A média dos números é ${media(lista)}<br>`
+    txt.innerHTML += `A soma dos números é ${soma(lista)}<br>`
+    txt.innerHTML += `Foram informados ${lista.length} números!`
+}
